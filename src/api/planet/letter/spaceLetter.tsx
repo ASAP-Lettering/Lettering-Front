6190be9fe7728b6caf10acdff3df0e5cc9ad9df1
yyypearl <hor629@naver.com>
2024-10-03 15:51:54 +0900

    ✨ feat(#17): Space API 연동

---
+++ src/api/planet/letter/spaceLetter.tsx
@@ -0,0 +1,41 @@
+import { authClient } from "@/api/client";
+
+// 행성 편지 목록 조회
+export const getPlanetLetterList = async ({
+  spaceId,
+  page,
+  size,
+}: {
+  spaceId: string;
+  page: number;
+  size: number;
+}) => {
+  return await authClient.get(
+    `/api/v1/spaces/${spaceId}/letters?page=${page}&size=${size}`
+  );
+};
+
+// 궤도 편지 행성으로 이동
+export const putLetterToPlanet = async (letterId: string) => {
+  return await authClient.put(`/api/v1/spaces/letters/${letterId}`);
+};
+
+// 행성 평지 독립 편지로 이동
+export const putLetterToIndep = async (letterId: string) => {
+  return await authClient.put(`/api/v1/spaces/letters/${letterId}/independent`);
+};
+
+// 행성 편지 상세 조회
+export const getPlanetLetter = async (letterId: string) => {
+  return await authClient.get(`/api/v1/spaces/letters/${letterId}`);
+};
+
+// 행성 편지 삭제
+export const deletePlanetLetter = async (letterId: string) => {
+  return await authClient.get(`/api/v1/spaces/letters/${letterId}`);
+};
+
+// 궤도 편지 목록 조회
+export const getOrbitLetter = async () => {
+  return await authClient.get(`/api/v1/letters/independent`);
+};
