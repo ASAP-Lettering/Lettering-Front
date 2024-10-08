2266f79695cbf7c018548beee4a1c5c1f54284f8
Seunghyo <shyo0218@naver.com>
2024-10-08 16:31:49 +0900

    🐛fix(#31): 배포 버그 fix(type 오류)

--- src/types/letter.ts
+++ src/types/letter.ts
@@ -14,7 +14,7 @@ export type LetterDetailType = {
   url?: string; // 편지 url
   templateType: number; // 편지 배경
   sender: string; // 편지를 보낸 사람
-  receive?: string; // 편지를 받은 사람
+  receiver?: string; // 편지를 받은 사람
   content: string; // 편지의 내용
   images: string[]; // 편지에 image 배열
   date: string; // 편지 날짜
