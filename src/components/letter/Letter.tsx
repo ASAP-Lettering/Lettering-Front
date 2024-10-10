cd95001d38c0ba4c739732e63d558ea474c5c1fe
yyypearl <hor629@naver.com>
2024-10-11 04:20:53 +0900

    ✨ feat(#24): Letter 컴포넌트 preview sender, receiver 타입 나누기

--- src/components/letter/Letter.tsx
+++ src/components/letter/Letter.tsx
@@ -6,7 +6,7 @@ import { theme } from "@/styles/theme";
 import { useRouter } from "next/navigation";
 import ConfirmModal from "../common/ConfirmModal";
 
-type showType = "preview" | "receive" | "send" | "url";
+type showType = "previewSend" | "previewReceive" | "receive" | "send" | "url";
 export type contentType = "one" | "all";
 
 interface LetterProps {
@@ -115,7 +115,7 @@ const Letter = (props: LetterProps) => {
         <>
           <TopContainer>
             <Name $showType={showType} $contentType={contentType}>
-              {showType === "send" ? `To. ${name}` : `From. ${name}`}
+              {`${showType === "send" ? `To. ` : `From. `} ${name}`}
             </Name>
             {!readOnly && (
               <button onClick={() => setIsPopup(!isPopup)}>
@@ -125,12 +125,14 @@ const Letter = (props: LetterProps) => {
           </TopContainer>
         </>
       )}
-      {showType === "send" && <Date $showType="send">{date}</Date>}
-      {showType === "preview" && (
+      {(showType === "send" || (showType === "previewSend" && isImage)) && (
+        <Date $showType="send">{date}</Date>
+      )}
+      {(showType === "previewReceive" || showType === "previewSend") && (
         <>
           <TopPreviewContainer>
             <Name $showType={showType} $contentType={contentType}>
-              From. {name}
+              {`${showType === "previewSend" ? `To. ` : `From. `} ${name}`}
             </Name>
           </TopPreviewContainer>
         </>
@@ -290,23 +292,23 @@ const ModalDate = styled.div`
 `;
 
 const UrlWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
 `;
 
 const UrlName = styled.div`
-    display: flex;
-    text-align: center;
-    ${(props: any) => props.theme.fonts.body06};
-    color: ${(props: any) => props.theme.colors.white};
+  display: flex;
+  text-align: center;
+  ${(props: any) => props.theme.fonts.body06};
+  color: ${(props: any) => props.theme.colors.white};
 `;
 
 const UrlDate = styled.div`
-    display: flex;
-    text-align: center;
-    ${(props: any) => props.theme.fonts.caption02};
-    color: ${(props: any) => props.theme.colors.gray500};
+  display: flex;
+  text-align: center;
+  ${(props: any) => props.theme.fonts.caption02};
+  color: ${(props: any) => props.theme.colors.gray500};
 `;
