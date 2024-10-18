f4f7d8f5ed2f7aee1dc6dfe0728dc6c4bde12669
Seunghyo <shyo0218@naver.com>
2024-10-18 16:58:46 +0900

    💄design(#39): drag 방지 (div, img) global 적용

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -37,6 +37,21 @@ li {
   list-style: none;
 }
 
+div,
+img
+{
+  //드래그방지
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -o-user-select: none;
+  user-select: none;
+  -webkit-user-drag: none;
+  -khtml-user-drag: none;
+  -moz-user-drag: none;
+  -o-user-drag: none;
+}
+
 button {
   cursor:pointer;
   border: none;
