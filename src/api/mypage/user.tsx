0416f26278651a5bf9f91c07fb4efe43b0abce15
Seunghyo <shyo0218@naver.com>
2024-10-19 17:09:07 +0900

    ✨feat(#39): 회원 정보 조회, 편지 tagtype 변경 연동

--- src/api/mypage/user.tsx
+++ src/api/mypage/user.tsx
@@ -11,3 +11,15 @@ export const logout = async (refreshToken: string) => {
 export const deleteUser = async () => {
   return await authClient.delete(`/api/v1/users`);
 };
+
+//화원 정보 조회
+export const getUserInfo = async () => {
+  return await authClient.get(`/api/v1/users/info/me`);
+};
+
+//생일 수정
+export const putUserBirthday = async (birthday: string) => {
+  return await authClient.put(`/api/v1/users/info/me/birthday`, {
+    birthday: birthday,
+  });
+};
