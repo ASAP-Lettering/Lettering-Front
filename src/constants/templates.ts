e25504588cbac11b5a9323dc2c4d0eb90e242a99
yyypearl <hor629@naver.com>
2024-12-22 00:10:45 +0900

    ✨ feat(#117): 편지지 템플릿 별도 상수 파일로 분리

---
+++ src/constants/templates.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_TEMPLATES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+export const EVENT_TEMPLATES = [10, 11, 12, 13];
+export const ALL_TEMPLATES = [...EVENT_TEMPLATES, ...DEFAULT_TEMPLATES];
