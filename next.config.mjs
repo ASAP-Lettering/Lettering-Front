2bc167e3d6c184f0325a58044321eaf95276c3c0
yyypearl <hor629@naver.com>
2025-02-06 21:37:41 +0900

    ⚙ chore(#142): console.log 블라인드 next.config 파일에서 설정

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,25 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ["lettering-images.s3.amazonaws.com"],
+    domains: ['lettering-images.s3.amazonaws.com']
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.minimize = true;
+      config.module.rules.push({
+        test: /\.(js|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['next/babel'],
+            plugins: [['transform-remove-console']]
+          }
+        }
+      });
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
