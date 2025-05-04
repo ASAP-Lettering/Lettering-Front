492d3c87deb896deb7b43f33993436b95e052a62
yyypearl <hor629@naver.com>
2025-05-05 01:06:30 +0900

    ⚙ chore(#168): next-pwa 설정을 pwa 키 아래로 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
+import withPlugins from 'next-compose-plugins';
 import withPWA from 'next-pwa';
+import typescript from 'next-plugin-graphql';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -12,6 +14,24 @@ const nextConfig = {
   }
 };
 
-export default withPWA({
-  dest: 'public'
-})(nextConfig);
+export default withPlugins(
+  [
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: 'public'
+        }
+      }
+    ],
+    [
+      typescript,
+      {
+        typescriptLoaderOptions: {
+          transpileOnly: false
+        }
+      }
+    ]
+  ],
+  nextConfig
+);
