144e75f3964091a3346857f8e5ead804168ebc5e
yyypearl <hor629@naver.com>
2025-05-04 23:10:28 +0900

    ♻ refactor(#165): 마이페이지 이메일 아이콘 constant로 리팩토링

--- src/constants/oauth.ts
+++ src/constants/oauth.ts
@@ -3,18 +3,21 @@ export const OAUTH = [
     key: 'naver',
     bgColor: '#03CF5D',
     icon: '/assets/icons/ic_naver.svg',
-    size: 26
+    size: 26,
+    profile: "/assets/icons/ic_naver.svg"
   },
   {
     key: 'google',
     bgColor: '#FFFFFF',
     icon: '/assets/icons/ic_google.svg',
-    size: 32
+    size: 32,
+    profile: "/assets/icons/ic_googler.svg"
   },
   {
     key: 'kakao',
     bgColor: '#FEE500',
     icon: '/assets/icons/ic_kakaotalk.svg',
-    size: 38
+    size: 38,
+    profile: '/assets/icons/ic_kakao_profile.svg',
   }
 ];
