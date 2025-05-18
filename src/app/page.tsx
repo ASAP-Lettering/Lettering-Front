4eca67d3a7e2170cf676ba782cdb9eeff4246a99
yyypearl <hor629@naver.com>
2025-05-18 10:06:36 +0900

    ✨ feat(#170): / 루트 경로 로딩 컴포넌트 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
-"use client";
+'use client';
 
-import { getAllSpaceName, getNewTokens } from "@/api/login/user";
-import Button from "@/components/common/Button";
-import KakaoShareButton from "@/components/common/KakaoShareButton";
-import Loader from "@/components/common/Loader";
-import {
-  clearInitUserToast,
-  clearTokens,
-  getAccessToken,
-  getCookie,
-  setCookie,
-} from "@/utils/storage";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import styled from "styled-components";
+import Loader, { LoaderContainer } from '@/components/common/Loader';
+import { getAccessToken } from '@/utils/storage';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 export default function Home() {
   const router = useRouter();
@@ -21,22 +12,17 @@ export default function Home() {
 
   useEffect(() => {
     if (!accessToken) {
-      router.push("/login");
+      router.push('/login');
     } else {
-      router.push("/planet");
+      router.push('/planet');
     }
   }, []);
 
   return (
     <Container>
-      {/* <ButtonContainer>
-        <Button
-          buttonType="primary"
-          text="로그아웃하기"
-          onClick={handleLogout}
-        ></Button>
-        <KakaoShareButton letterId="aa" />
-      </ButtonContainer> */}
+      <LoaderContainer>
+        <Loader />
+      </LoaderContainer>
     </Container>
   );
 }
@@ -48,20 +34,4 @@ const Container = styled.div`
   height: 100vh;
   padding: 25px;
   background: ${(props) => props.theme.colors.bg};
-`;
-
-const LoaderContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  min-height: 600px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const ButtonContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
 `;
